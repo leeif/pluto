e2d40a9ea3dccf91c803306418b1f0a0dcc824f6
leeif <geeklyf92610@gmail.com>
2019-09-08 21:15:55 +0900

    Feature(test): add integration test

---
+++ integration/intergration.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"net/http"
+	"os/exec"
+	"time"
+)
+
+type testCase struct {
+	Name string
+	Func func() error
+}
+
+var testCases = []testCase{
+	{
+		Name: "testHealthCheck",
+		Func: testHealthCheck,
+	},
+	{
+		Name: "testMailRegisterBadRequest",
+		Func: testMailRegisterBadRequest,
+	},
+	{
+		Name: "testMailRegisterOK",
+		Func: testMailRegisterOK,
+	},
+	{
+		Name: "testRegisterVerifyOK",
+		Func: testRegisterVerifyOK,
+	},
+	{
+		Name: "testMailLoginOK",
+		Func: testMailLoginOK,
+	},
+	{
+		Name: "testGetUserInfo",
+		Func: testGetUserInfo,
+	},
+	{
+		Name: "testGetPublicKey",
+		Func: testGetPublicKey,
+	},
+	{
+		Name: "testRefreshAccessToken",
+		Func: testRefreshAccessToken,
+	},
+	{
+		Name: "testResetPassword",
+		Func: testResetPassword,
+	},
+	{
+		Name: "testNewPasswordLogin",
+		Func: testNewPasswordLogin,
+	},
+	{
+		Name: "testPasswordResetResultFail",
+		Func: testPasswordResetResultFail,
+	},
+	{
+		Name: "testPasswordResetResultOK",
+		Func: testPasswordResetResultOK,
+	},
+	{
+		Name: "testPasswordResetFail",
+		Func: testPasswordResetFail,
+	},
+	{
+		Name: "testPasswordResetOK",
+		Func: testPasswordResetOK,
+	},
+}
+
+func testHealthCheck() error {
+	url := "http://localhost:8010/healthcheck"
+	for i := 0; i < 10; i++ {
+		log.Printf("try count: %v\n", i)
+		resp, err := http.Get(url)
+		time.Sleep(time.Duration(10) * time.Second)
+		if err != nil {
+			continue
+		}
+		if resp.StatusCode == http.StatusOK {
+			return nil
+		}
+	}
+	return errors.New("Healthcheck failed")
+}
+
+func main() {
+	log.Println("docker-compose -f docker/docker-compose.yml up -d")
+	cmd := exec.Command("docker-compose", "-f", "docker/docker-compose.yml", "up", "-d")
+	cmd.Start()
+	time.Sleep(time.Duration(10) * time.Second)
+	defer func() {
+		log.Println("docker-compose -f docker/docker-compose.yml down")
+		cmd := exec.Command("docker-compose", "-f", "docker/docker-compose.yml", "down")
+		cmd.Start()
+		time.Sleep(time.Duration(10) * time.Second)
+	}()
+	for _, tc := range testCases {
+		log.Printf("====== start %v ======\n", tc.Name)
+		err := tc.Func()
+		if err != nil {
+			log.Panicf("Error: %v", err)
+		}
+		log.Printf("test ok\n")
+		log.Printf("====== end %v ======\n\n", tc.Name)
+	}
+}
---
+++ integration/reset_password.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/leeif/pluto/config"
+	"github.com/leeif/pluto/utils/jwt"
+	"github.com/leeif/pluto/utils/rsa"
+
+	"github.com/leeif/pluto/datatype/request"
+)
+
+func testResetPassword() error {
+	cfg := config.Config{}
+	cfg.RSA = &config.RSAConfig{}
+	name := "ids_rsa_test"
+	cfg.RSA.Name = &name
+	path := "./docker"
+	cfg.RSA.Path = &path
+	if err := rsa.Init(&cfg); err != nil {
+		return fmt.Errorf("Expect no error, but %v", err)
+	}
+	token, perror := jwt.GenerateJWT(jwt.Head{Type: jwt.PASSWORDRESET, Alg: jwt.ALGRAS},
+		&jwt.PasswordResetPayload{Mail: "test@gmail.com"}, 60*60)
+
+	if perror != nil {
+		return fmt.Errorf("Expect no error, but %v", perror.LogError)
+	}
+
+	url := "http://localhost:8010/api/user/password/reset"
+	payload := request.ResetPassword{
+		Token:    base64.StdEncoding.EncodeToString([]byte(token)),
+		Password: "test_new",
+	}
+	b, err := json.Marshal(payload)
+	if err != nil {
+		return fmt.Errorf("Expect no error, but %v", err)
+	}
+
+	if resp, err := http.Post(url, "application/json", bytes.NewReader(b)); err != nil {
+		return fmt.Errorf("Expect no error, but %v", err)
+	} else {
+		if resp.StatusCode != http.StatusOK {
+			return fmt.Errorf("%v request expect ok (200) status code, but %v", url, resp.StatusCode)
+		}
+	}
+	return nil
+}
+
+func testNewPasswordLogin() error {
+	url := "http://localhost:8010/api/user/login"
+	payload := request.MailLogin{
+		Mail:     "test@gmail.com",
+		Password: "test",
+		DeviceID: "xxx",
+		AppID:    "xxx",
+	}
+
+	// login with old password
+	b, err := json.Marshal(payload)
+	if err != nil {
+		return fmt.Errorf("Expect no error, but %v", err)
+	}
+
+	resp, err := http.Post(url, "application/json", bytes.NewReader(b))
+
+	if err != nil {
+		return fmt.Errorf("Expect no error, but %v", err)
+	}
+
+	if resp.StatusCode != http.StatusForbidden {
+		return fmt.Errorf("%v request expect forbidden (403) status code, but %v", url, resp.StatusCode)
+	}
+
+	// login with new password
+	payload.Password = "test_new"
+	b, err = json.Marshal(payload)
+	if err != nil {
+		return fmt.Errorf("Expect no error, but %v", err)
+	}
+
+	resp, err = http.Post(url, "application/json", bytes.NewReader(b))
+
+	if err != nil {
+		return fmt.Errorf("Expect no error, but %v", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("%v request expect ok (200) status code, but %v", url, resp.StatusCode)
+	}
+
+	return nil
+}
